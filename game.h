3e9503f9f314b95c666cc8628ff397a524a099b9
minnielach <165672835+minnielach@users.noreply.github.com>
2026-04-10 00:14:21 +0930

    Completed the game.h for declartions which will allow the game class to track current player with a boool, create shuffle deck,  and switch turns etc.

--- game.h
+++ game.h
@@ -1,5 +1,32 @@
-
+#pragma once 
+#include "player.h"
+#include "card.h"
 
 class Game {
+    private:
+    Player player1;
+    Player player2;
 
+    CardCollection deck;
+    CardCollection discardPile;
+
+    bool player1Turn;
+
+    int countTurn;
+    int round;
+
+    public:
+    Game();
+
+    void createDeck();
+    void shuffleDeck();
+
+    void playGame();
+    void currentTurn();
+    void switchTurns();
+
+    Player& getPlayer1();
+    Player& getPlayer2();
+
+    CardCollection& getDiscardPile();
 };
